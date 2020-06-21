9c14e74a0546e35fc49c66576f5550f50fbd9ba8
studTon <bra.17.ess@gmail.com>
2020-06-20 21:58:37 -0300

    added explanation about variable

--- Project euler/Problem 3.c
+++ Project euler/Problem 3.c
@@ -3,14 +3,16 @@
 What is the largest prime factor of the number 600851475143 ?*/
 #include <stdio.h>
 int main(){
-	int num = 13195;
+	int long long num = 189523; /*num is the number we want to
+	discover the largest prime factor.*/
 	int prime;
 	int i;
 	int div;
 	int aux;
 	
+	printf("The number: %d\nPrime factors:\n", num);
 	prime = 2;
-	while(prime < 1000){
+	while(prime < 189524){
 		i = 0; 
 		div = 1;
 		while(div <= prime){
@@ -18,9 +20,19 @@ int main(){
 				i++;
 			div++;
 		}
-		if(i == 2)
-			printf("%d\n", prime);
-		prime++;
+		if(i == 2){
+			if(num % prime == 0){
+				num /= prime;
+				printf("%d\n", prime);
+				if(num == 1)
+					printf("Largest prime factor: %d\n", prime);
+			}
+			else
+				prime++;
+		}
+		else
+			prime++;
+		
 	}
 	
 	
